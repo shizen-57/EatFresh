cc2a267fea0aa139ce766ae592a0381824e163bc
shizen-57 <sifatts2@gmail.com>
2025-01-01 03:36:39 +0600

    navigation & list scroll

--- App.js
+++ App.js
@@ -1,15 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { ResturantsScreen } from './src/features/resturants/screens/resturants.screen';
+import { StatusBar as ExpoStatusBar } from "expo-status-bar";
+import React from "react";
+import { Text } from "react-native";
+import { ThemeProvider } from "styled-components/native";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Ionicons } from "@expo/vector-icons";
+
+import {
+  useFonts as useOswald,
+  Oswald_400Regular,
+} from "@expo-google-fonts/oswald";
+import { useFonts as useLato, Lato_400Regular } from "@expo-google-fonts/lato";
+
+import { theme } from "./src/infrastructure/theme";
+import { RestaurantsScreen } from "./src/features/resturants/screens/restaurants.screen";
+
+import { SafeArea } from "./src/components/utility/safe_area.component";
+
+const Tab = createBottomTabNavigator();
+
+const Settings = () => (
+  <SafeArea>
+    <Text>Settings</Text>
+  </SafeArea>
+);
+const Map = () => (
+  <SafeArea>
+    <Text>Map</Text>
+  </SafeArea>
+);
 
 export default function App() {
+  const [oswaldLoaded] = useOswald({
+    Oswald_400Regular,
+  });
+
+  const [latoLoaded] = useLato({
+    Lato_400Regular,
+  });
+
+  if (!oswaldLoaded || !latoLoaded) {
+    return null;
+  }
 
   return (
     <>
-    <ResturantsScreen/>
-    <StatusBar style='auto' />
+      <ThemeProvider theme={theme}>
+      <NavigationContainer>
+      <Tab.Navigator
+    screenOptions={({ route }) => ({
+      tabBarIcon: ({ color, size }) => {
+        let iconName;
+
+        if (route.name === "Restaurants") {
+          iconName = "restaurant"; // Fixed icon name
+        } else if (route.name === "Settings") {
+          iconName = "settings"; // Fixed icon name
+        } else if (route.name === "Map") {
+          iconName = "map"; // Fixed icon name
+        }
+
+        return <Ionicons name={iconName} size={size} color={color} />;
+      },
+      tabBarActiveTintColor: "tomato",
+      tabBarInactiveTintColor: "gray",
+      tabBarStyle: { backgroundColor: "white" },
+    })}
+  >
+            <Tab.Screen name="Restaurants" component={RestaurantsScreen} />
+            <Tab.Screen name="Map" component={Map} />
+            <Tab.Screen name="Settings" component={Settings} />
+          </Tab.Navigator>
+        </NavigationContainer>
+      </ThemeProvider>
+      <ExpoStatusBar style="auto" />
     </>
   );
 }
-
 
